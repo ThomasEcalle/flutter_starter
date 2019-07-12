099d950037de5aee72df3d70284e48d476faf0fc
Ecalle Thomas <thomasecalle@hotmail.fr>
2019-07-12 15:44:30 +0200

    Put SplashScreen in Scaffold

--- lib/ui/splashscreen.dart
+++ lib/ui/splashscreen.dart
@@ -4,10 +4,12 @@ import 'package:flutter_model/core/translations.dart';
 class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Theme.of(context).primaryColor,
-      child: Center(
-        child: Text(Translations.of(context).text("app_title")),
+    return Scaffold(
+      body: Container(
+        color: Theme.of(context).primaryColor,
+        child: Center(
+          child: Text(Translations.of(context).text("app_title")),
+        ),
       ),
     );
   }
