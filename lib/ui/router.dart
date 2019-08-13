07486640d06e7c27a64f6ba9de459ac2a853865b
David Fournier <david.fournier@smartnsoft.com>
2019-08-13 15:12:25 +0200

    Create AppTheme subcategories, remove warnings and create a default route for the router

--- lib/ui/router.dart
+++ lib/ui/router.dart
@@ -6,24 +6,21 @@ import 'authenticated/post_detail/post_detail.dart';
 class Router {
   static Route<dynamic> generateRoute(RouteSettings routeSettings) {
     final dynamicArguments = routeSettings.arguments;
+    // Instantiate the default route here
+    Widget route = Scaffold(
+      body: Center(
+        child: Text('No route defined for ${routeSettings.name}'),
+      ),
+    );
     switch (routeSettings.name) {
       case PostDetail.routeName:
         if (dynamicArguments is PostDetailArguments) {
-          return MaterialPageRoute(builder: (BuildContext context) {
-            return PostDetail(
-              post: dynamicArguments.post,
-            );
-          });
+          route = PostDetail(
+            post: dynamicArguments.post,
+          );
         }
         break;
-      default:
-        return MaterialPageRoute(builder: (_) {
-          return Scaffold(
-            body: Center(
-              child: Text('No route defined for ${routeSettings.name}'),
-            ),
-          );
-        });
     }
+    return MaterialPageRoute(builder: (BuildContext context) => route);
   }
 }
