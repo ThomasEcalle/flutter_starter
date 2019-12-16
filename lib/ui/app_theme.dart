471db16aa111ea889a650fa812853b847bad9f2d
Ecalle <thomasecalle@hotmail.fr>
2019-12-16 16:10:42 +0100

    Refactor AppTheme to add types

--- lib/ui/app_theme.dart
+++ lib/ui/app_theme.dart
@@ -2,41 +2,45 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class _Colors {
-  get black => Color(0xff000000);
-  get white => Color(0xffffffff);
-  get grey => Color(0xff9e9e9e);
+  Color get black => Color(0xff000000);
+
+  Color get white => Color(0xffffffff);
+
+  Color get grey => Color(0xff9e9e9e);
 }
 
 class _Values {
   get defaultBorderRadius => BorderRadius.circular(5.0);
 }
 
+class _Icons {}
+
 class _TextStyles {
   final _Colors colors;
 
   _TextStyles(this.colors);
 
-  get title => TextStyle(
+  TextStyle get title => TextStyle(
         color: Colors.grey,
         fontSize: 30,
         fontWeight: FontWeight.bold,
         letterSpacing: -0.2,
       );
 
-  get button => TextStyle(
+  TextStyle get button => TextStyle(
         color: colors.white,
         fontSize: 15,
         fontWeight: FontWeight.w500,
         letterSpacing: 0.94,
       );
 
-  get display1 => TextStyle(
+  TextStyle get display1 => TextStyle(
         color: colors.black,
         fontWeight: FontWeight.bold,
         fontSize: 18,
       );
 
-  get display2 => TextStyle(
+  TextStyle get display2 => TextStyle(
         color: colors.grey,
         fontSize: 14,
       );
@@ -47,6 +51,8 @@ class AppTheme extends InheritedWidget {
   final _Colors colors = _Colors();
 
   final _Values values = _Values();
+
+  final _Icons icons = _Icons();
 
   _TextStyles textStyles;
 
